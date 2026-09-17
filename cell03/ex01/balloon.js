4d9349bbbb600d3c064b6be1b721bacb0c705cc7
K-27 <kauananddradeesilva@gmail.com>
2026-09-17 16:03:03 -0300

    Resolução balão

--- cell03/ex01/balloon.js
+++ cell03/ex01/balloon.js
@@ -1,13 +1,46 @@
 const cores = ['red', 'green', 'blue']
 const balao = document.getElementById('balao');
+const estilo = window.getComputedStyle(balao);
 
 let indice = 0;
+let largura = parseInt(estilo.width);
+let altura = parseInt(estilo.height);
 
 	balao.addEventListener('click', function() {
-	balao.style.width = (largura + 50) + 'px';
-	balao.style.height = (altura + 50) + 'px';
+
+	indice = (indice + 1) % cores.length;
+	balao.style.backgroundColor = cores[indice];
 	
+		largura += 10;
+		balao.style.width = largura + 'px';
+
+		altura += 10;
+		balao.style.height = altura + 'px';
+
+		if (largura >= 420) {
+			balao.style.width = '200px';
+			balao.style.height = '200px';
+			largura = 200;
+			altura = 200;
+		}
+
+})
+
+balao.addEventListener('mouseleave', function() {
+
+		indice = (indice - 1) % cores.length;
 	
-	balao.style.backgroundColor = cores [indice]
-	indice = (indice + 1) % cores.lenght;
-});
+		if (indice < 0) {
+			indice = 2;	
+		}
+
+		balao.style.backgroundColor = cores[indice];
+
+		largura -= 5;
+		balao.style.width = largura + 'px';
+
+		altura -= 5;
+		balao.style.height = altura + 'px';
+		
+})
+;
